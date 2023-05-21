1b5bdd313e9df242b78bdb74d567255d9e6beb93
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-05-21 17:21:21 +0600

    bug fixed for deployment

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors())
 app.use(express.json());
 
 
@@ -32,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    
+    // await client.connect();
 
     const toysCollection = client.db('actionCon').collection('toys');
 
@@ -52,7 +46,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
